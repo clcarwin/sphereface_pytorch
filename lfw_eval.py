3a4c230c898a4937eb9f903a15fb0811bcf0ad79
Sayandip Dutta <sayandip199309@gmail.com>
2022-02-02 02:10:05 +0530

    Change inner loop to use enumerate
    The inner loop used `range(len(imglist))`, this iterates over the entire
    length twice. Using `enumerate` saves one complete iteration.
    
    Furthermore, inside the inner loop `__getitem__` and `__setitem__` were
    being called twice in each iteration. Reduces `__getitem__` calls to 0,
    as it is already being taken care by for loop, and reduced `__setitem__`
    to 1.

--- lfw_eval.py
+++ lfw_eval.py
@@ -103,9 +103,9 @@ with open('data/pairs.txt') as f:
         img2 = alignment(cv2.imdecode(np.frombuffer(zfile.read(name2),np.uint8),1),landmark[name2])
 
         imglist = [img1,cv2.flip(img1,1),img2,cv2.flip(img2,1)]
-        for i in range(len(imglist)):
-            imglist[i] = imglist[i].transpose(2, 0, 1).reshape((1,3,112,96))
-            imglist[i] = (imglist[i]-127.5)/128.0
+        for i, image in imglist:
+            image = image.transpose(2, 0, 1).reshape((1,3,112,96))
+            imglist[i] = (image - 127.5) / 128.0
 
         img = np.vstack(imglist)
         img = Variable(torch.from_numpy(img).float(),volatile=True).cuda()
